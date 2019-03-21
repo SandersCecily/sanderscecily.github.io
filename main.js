3f63280926cee9fcae06a31b94a6e5578d1e77a0
Cecily <Sanders.Cecily@gmail.com>
2019-03-21 16:49:54 -0400

    New css, and file structure

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-/*
-The student has met all of the requirements under "Employer Competitive." 
-This means not only do they have a polished portfolio page linking to at least 3 projects, 
-but they have also updated their GitHub repos with detailed READMEs and their LinkedIn. 
-Only award an A+ if they've also refactored code from an earlier assignment. 
-You should ask them if they're comfortable letting you share their portfolio with the rest of the class, 
-*especially* if they aren't among your most technically strong students 
-to encourage other students who may be struggling with impostor syndrome.
-*/
-
-
+//function to show accordian
 $(".card-header").click(function(){
     if($("#others").hasClass("collapse show")){
         $("#others").removeClass();
@@ -19,3 +9,7 @@ $(".card-header").click(function(){
         $("#others").addClass("collapse show");
     }
 });
+
+function populateProjects(){
+    
+}
